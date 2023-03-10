6db3ff6e8a2c0ea2827ca89e6286723ee64cd5a0
Batjimin <101635924+Batjimin@users.noreply.github.com>
2023-03-10 13:42:56 +0900

    Author Added

---
+++ DataStruc1.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <stdio.h>
+int main()
+{
+	char c = 'A';
+	char* pc = &c;
+	char** ppc = &pc;
+	printf("%p %p\n", pc, pc + 1);
+	printf("%p %p\n", ppc, ppc + 1);
+	printf("%p %p\n", &c, &c + 1);
+	printf("%p %p\n", &pc, &pc + 1);
+	printf("%p %p\n", &ppc, &ppc + 1);
+	return 0;
+}
+
+//202201541 컴퓨터공학부 박지민
