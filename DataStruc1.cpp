6c7b308a6a28f2396f4019443af9654a236a75da
LeEHaNa1 <127467192+LeEHaNa1@users.noreply.github.com>
2023-03-24 13:50:04 +0900

    Add code
    문자 바꾸고 코드 한 개 더 추가했어요.

--- DataStruc1.cpp
+++ DataStruc1.cpp
@@ -2,10 +2,11 @@
 #include <stdio.h>
 int main()
 {
-	char x = 'A';
+	char x = 'D';
 	char* px = &x;
 	char** ppx = &px;
 	printf("%p %p\n", px, px + 1); // x의 바이트 수가 1이므로 1 증가
+	printf("%p %p\n", px, px + 2);
 	printf("%p %p\n", ppx, ppx + 1); // px의 바이트 수가 4. 따라서 그의 포인터인 ppx는 +1 시 4 증가
 	printf("%p %p\n", &x, &x + 1); // x의 주소에 1 증가 
 	printf("%p %p\n", &px, &px + 1); // 상단의 설명 참고.
